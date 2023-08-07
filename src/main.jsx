78354babed4727d733d0fea205baaffafe908348
Mike Vardy <mike.vardy@redspace.com>
2023-08-07 04:40:07 -0400

    inital commit

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { createRoot } from 'react-dom';
+import App from './App';
+
+// Codesandbox does not properly parse @imports so we import them here
+import './styles/root.css';
+import './styles/fonts.css';
+import './styles/typography.css';
+import './styles/css-reset.css';
+import './styles/variables.css';
+import './styles/ci-v11-variables.css';
+import './styles/util.css';
+import './styles/forms.css';
+import './styles/button.css';
+
+createRoot(document.getElementById('root')).render(<App />);
